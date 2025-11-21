b7229944e0468a871d658c4e3d4a6c9959871e2a
Elisey <thefucingsokol.gg@yandex.ru>
2025-11-21 22:24:59 +0500

    fix

--- cmd/gosling/main.go
+++ cmd/gosling/main.go
@@ -1,15 +1,15 @@
 package main
 
 import (
-    "fmt"
-    "os"
+	"fmt"
+	"os"
 
-    "github.com/ElishaFlacon/fast-sobes-auth/pkg/gosling"
+	"github.com/ElishaFlacon/fast-sobes-auth/pkg/gosling"
 )
 
 func main() {
-    if err := gosling.Execute(); err != nil {
-        fmt.Fprintf(os.Stderr, "Error: %v\n", err)
-        os.Exit(1)
-    }
+	if err := gosling.Execute(); err != nil {
+		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+		os.Exit(1)
+	}
 }
